a43a5280deb65d8f80622bc2f4f6ddab6b81ccd2
gaoshaoyun <276185876@qq.com>
2020-11-04 17:17:36 +0800

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {Component} from "react";
 
-function HomePage(props) {
-  return (
-    <div>
-      <h3>HomePage</h3>
-    </div>
-  );
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>HomePage</h3>
+      </div>
+    );
+  }
 }
-export default HomePage;
